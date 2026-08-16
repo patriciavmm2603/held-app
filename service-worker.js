eec442ddfc2ce65f9ea713f589470cbc6cf457da
patriciavmm2603 <munoz.pat@outlook.com>
2026-08-16 17:47:44 -0400

    Refresh Held milestone notifications

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="held-v24";
+const CACHE_NAME="held-v25";
 const CORE_ASSETS=["./","./index.html","./manifest.webmanifest","./assets/held-app-icon.png"];
 
 self.addEventListener("install",event=>{
