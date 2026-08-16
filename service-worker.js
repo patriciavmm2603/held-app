4d2fab603be2309613d97aa79be924f9d242a6a3
patriciavmm2603 <munoz.pat@outlook.com>
2026-08-16 17:23:22 -0400

    Refresh Held notification cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="held-v18";
+const CACHE_NAME="held-v21";
 const CORE_ASSETS=["./","./index.html","./manifest.webmanifest","./assets/held-app-icon.png"];
 
 self.addEventListener("install",event=>{
