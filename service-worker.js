78b3b4942923a000c6eef92adaa514f59c9b3aeb
patriciavmm2603 <munoz.pat@outlook.com>
2026-08-17 09:32:43 -0400

    Refresh Held cache for couple connection features

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="held-v27";
+const CACHE_NAME="held-v28";
 const CORE_ASSETS=["./","./index.html","./manifest.webmanifest","./assets/held-app-icon.png"];
 
 self.addEventListener("install",event=>{
