6d249f3a7107498a4c2d87dc4d65dc273de639e8
patriciavmm2603 <munoz.pat@outlook.com>
2026-08-17 12:20:47 -0400

    Refresh Held v34 secure conversation cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="held-v33";
+const CACHE_NAME="held-v34";
 const CORE_ASSETS=["./","./index.html","./manifest.webmanifest","./assets/held-app-icon.png"];
 
 self.addEventListener("install",event=>{
